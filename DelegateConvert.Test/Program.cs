671c6dec8c5031c6d8c3729111216d93e589831c
J@School <darkie@j-kit.de>
2017-10-19 12:09:09 +0200

    Created Performancetests [TODO: Own]

--- DelegateConvert.Test/Program.cs
+++ DelegateConvert.Test/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DelegateConvert;
 using System.Diagnostics;
 using JSocket.Utilities.Generic.Delegate.DelegateConvert;
+using System.Reflection;
 
 namespace DelegateConvert.Test
 {
@@ -13,6 +14,11 @@ namespace DelegateConvert.Test
     {
         private static void Main(string[] args)
         {
+            TimedTests.RawTest();
+            TimedTests.MethodInvokeTest();
+            TimedTests.DynamicInvokeTest();
+
+            Console.ReadLine();
             ActionTest();
             FunctionTest();
             Debugger.Break();
@@ -37,6 +43,66 @@ namespace DelegateConvert.Test
             {
                 Console.WriteLine("Nay");
             }
+        }
+    }
+    public class TimedTests
+    {
+        private const int Loops = 5000000;
+
+        private static MethodInfo GetMethodInfo()
+        {
+            return typeof(DummyCaller).GetMethod("Sum");
+        }
+
+        public static void RawTest()
+        {
+            var instance = new DummyCaller();
+            instance.Sum(1, 2, 3, 4, 5, 6, 7, 8);
+
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < Loops; i++)
+            {
+                instance.Sum(1, 2, 3, 4, 5, 6, 7, 8);
+            }
+            Console.WriteLine($"{Loops} Rawtests took {sw.Elapsed.TotalMilliseconds} ms");
+        }
+
+        public static void MethodInvokeTest()
+        {
+            var instance = new DummyCaller();
+            var method = GetMethodInfo();
+            var invokeParams = new object[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < Loops; i++)
+            {
+                // instance.Sum(1, 2, 3, 4, 5, 6, 7, 8);
+
+                method.Invoke(instance, invokeParams);
+            }
+            Console.WriteLine($"{Loops} MethodInvokeTest took {sw.Elapsed.TotalMilliseconds} ms");
+        }
+
+        public static void DynamicInvokeTest()
+        {
+            var instance = new DummyCaller();
+            var method = GetMethodInfo();
+            var invokeParams = new object[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var dele = method.CreateDelegate(typeof(Func<int, int, int, int, int, int, int, int, int>), instance);
+
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < Loops; i++)
+            {
+                method.Invoke(instance, invokeParams);
+            }
+            Console.WriteLine($"{Loops} DynamicInvokeTest took {sw.Elapsed.TotalMilliseconds} ms");
+        }
+    }
+    public class DummyCaller
+    {
+        public int Sum(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8)
+        {
+            return p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8;
         }
     }
 }
